463b8e37615ae46e796f747989abd73715daaf2a
Jamnic98 <jamiestimpson30@gmail.com>
2024-03-27 18:59:41 +0000

    fix

--- src/database.ts
+++ src/database.ts
@@ -1,8 +1,11 @@
 import mongoose from 'mongoose'
 
-const MONGODB_URI = process.env.MONGODB_URI || ''
+type ExitHandlerOptions = {
+	cleanup?: boolean
+	exit?: boolean
+}
 
-const exitHandler = (options: { cleanup?: boolean; exit?: boolean }) => {
+const exitHandler = (options: ExitHandlerOptions) => {
 	if (options.cleanup) {
 		mongoose.connection.close()
 	}
@@ -11,23 +14,24 @@ const exitHandler = (options: { cleanup?: boolean; exit?: boolean }) => {
 	}
 }
 
-process.stdin.resume() //so the program will not close instantly
-//do something when app is closing
-process.on('exit', exitHandler.bind(null, { cleanup: true }))
-//catches ctrl+c event
-process.on('SIGINT', exitHandler.bind(null, { exit: true }))
-// catches "kill pid" (for example: nodemon restart)
-process.on('SIGUSR1', exitHandler.bind(null, { exit: true }))
-process.on('SIGUSR2', exitHandler.bind(null, { exit: true }))
-
-//catches uncaught exceptions
-process.on('uncaughtException', exitHandler.bind(null, { exit: true }))
-
 // configure mongoose and connect to database
-export default async () => {
+export const connectToDB = async (mongoDBConnectionURI: string) => {
+	process.stdin.resume() //so the program will not close instantly
+	//do something when app is closing
+	process.on('exit', exitHandler.bind(null, { cleanup: true }))
+	//catches ctrl+c event
+	process.on('SIGINT', exitHandler.bind(null, { exit: true }))
+	// catches "kill pid" (for example: nodemon restart)
+	process.on('SIGUSR1', exitHandler.bind(null, { exit: true }))
+	process.on('SIGUSR2', exitHandler.bind(null, { exit: true }))
+	//catches uncaught exceptions
+	process.on('uncaughtException', exitHandler.bind(null, { exit: true }))
+
 	try {
-		await mongoose.connect(MONGODB_URI, {
+		await mongoose.connect(mongoDBConnectionURI, {
 			autoIndex: false,
+			retryWrites: true,
+			writeConcern: { w: 'majority' },
 		})
 		console.log('Database connection established')
 	} catch (_error) {
---
+++ src/utils/strava.utils.ts
@@ -0,0 +1,25 @@
+import axios from 'axios'
+
+import { getStravaToken } from '.'
+import { Run } from '../store'
+
+export const fetchStravaActivities = async (
+	after: number = 0
+): Promise<Run[] | null> => {
+	try {
+		const token = await getStravaToken()
+		if (token) {
+			const response = await axios.get(
+				'https://www.strava.com/api/v3/athlete/activities',
+				{
+					headers: { Authorization: `Bearer ${token.access_token}` },
+					params: { after },
+				}
+			)
+			return response.data
+		}
+	} catch (error) {
+		console.error(error)
+	}
+	return null
+}
