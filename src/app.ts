5a071354fb19dd6e50dfdddb371f815419f5a1cf
Jamnic98 <jamiestimpson30@gmail.com>
2024-04-30 22:20:50 +0100

    test

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,6 @@ import * as dotenv from 'dotenv'
 import app from './server'
 import { connectToDB } from './database'
 import { addRunsToDatabase } from './utils'
-import { exit } from 'process'
 
 dotenv.config()
 
@@ -14,13 +13,5 @@ const MONGODB_URI = process.env.MONGODB_URI || ''
 // schedule task to run each day at midnight
 cron.schedule('0 0 * * *', async () => await addRunsToDatabase())
 
-const runApp = async () => {
-	try {
-		await connectToDB(MONGODB_URI)
-		app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`))
-	} catch (error) {
-		console.error(error)
-	}
-}
-
-runApp()
+connectToDB(MONGODB_URI)
+app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`))
