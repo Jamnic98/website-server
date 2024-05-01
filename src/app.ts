26bbbc188ca14f19a279fddc2692eff89539b3c6
Jamnic98 <jamiestimpson30@gmail.com>
2024-05-01 15:07:07 +0100

    test

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,7 @@ import { addRunsToDatabase } from './utils'
 
 dotenv.config()
 
-const PORT = process?.env?.PORT || 5000
+const PORT = process?.env?.PORT || 8080
 const MONGODB_URI = process.env.MONGODB_URI || ''
 
 // schedule task to run each day at midnight
@@ -15,7 +15,4 @@ cron.schedule('0 0 * * *', async () => await addRunsToDatabase())
 
 connectToDB(MONGODB_URI)
 
-const host = '127.0.0.1'
-app.listen(Number(PORT), host, () =>
-	console.log(`Server listening on ${host}:${PORT}`)
-)
+app.listen(Number(PORT), () => console.log(`Server listening on port ${PORT}`))
--- src/database.ts
+++ src/database.ts
@@ -1,32 +1,7 @@
 import mongoose from 'mongoose'
 
-// type ExitHandlerOptions = {
-// 	cleanup?: boolean
-// 	exit?: boolean
-// }
-
-// const exitHandler = (options: ExitHandlerOptions) => {
-// 	if (options.cleanup) {
-// 		mongoose.connection.close()
-// 	}
-// 	if (options.exit) {
-// 		process.exit()
-// 	}
-// }
-
 // configure mongoose and connect to database
 export const connectToDB = async (mongoDBConnectionURI: string) => {
-	// process.stdin.resume() //so the program will not close instantly
-	// //do something when app is closing
-	// process.on('exit', exitHandler.bind(null, { cleanup: true }))
-	// //catches ctrl+c event
-	// process.on('SIGINT', exitHandler.bind(null, { exit: true }))
-	// // catches "kill pid" (for example: nodemon restart)
-	// process.on('SIGUSR1', exitHandler.bind(null, { exit: true }))
-	// process.on('SIGUSR2', exitHandler.bind(null, { exit: true }))
-	// //catches uncaught exceptions
-	// process.on('uncaughtException', exitHandler.bind(null, { exit: true }))
-
 	try {
 		await mongoose.connect(mongoDBConnectionURI, {
 			autoIndex: false,
